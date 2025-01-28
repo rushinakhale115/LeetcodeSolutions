1be6e30c349ede3a35361df968d54de3f4bd5db8
Rushikesh Nakhale <rushikesh.nakhale23@pccoepune.org>
2025-01-28 23:52:28 +0530

    Add files via upload

---
+++ ReverseLinkedlist.java
@@ -0,0 +1,58 @@
+
+
+  public class ListNode {
+     int val;
+      ListNode next;
+      ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+ 
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode curr = head;
+        ListNode prev = null;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+        
+    }
+}
+
+public class ReverseLinkedlist {
+    public static void main(String[] args) {
+        // Creating a sample linked list: 1 -> 2 -> 3 -> 4 -> 5
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.println("Original List:");
+        printList(head);
+
+        // Creating an instance of Solution to call the reverseList method
+        Solution solution = new Solution();
+        ListNode reversedHead = solution.reverseList(head);
+
+        System.out.println("\nReversed List:");
+        printList(reversedHead);
+    }
+
+    // Helper function to print the linked list
+    public static void printList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
