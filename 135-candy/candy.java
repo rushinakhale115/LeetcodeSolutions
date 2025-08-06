15190f693dc31196169a815d1e4a07c451ecde4c
Rushikesh Nakhale <rushikesh.nakhale23@pccoepune.org>
2025-08-06 15:45:16 +0530

    Time: 3 ms (79.55%) | Memory: 46.2 MB (24.27%) - LeetSync

---
+++ 135-candy/candy.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n=ratings.length;
+        int [] candies=new int[n];
+        int total=0;
+        Arrays.fill(candies,1);
+
+        for(int i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1]){
+                candies[i]=candies[i-1]+1;
+            }
+        }
+
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]){
+                candies[i]=Math.max(candies[i],candies[i+1]+1);
+            }
+        }
+        for(int i=0;i<candies.length;i++){
+            total+=candies[i];
+        }
+        return total;
+    }
+}
