58faeddd08310ce92747565927c87c894da28cff
Rushikesh Nakhale <rushikesh.nakhale23@pccoepune.org>
2025-08-25 16:52:27 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (18.39%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,16 +1,17 @@
 class Solution {
     public int climbStairs(int n) {
-       if(n==1 ||n==0){
-        return 1;
-       }
-       int dp[] = new int[n+1];
-        dp[0] = dp[1] = 1;
-        for(int i = 2; i <= n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-
-        return dp[n];
-
         
+
+            int [] arr=new int[n+1];
+            arr[0]=1;
+            arr[1]=1;
+            for(int i=2;i<=n;i++){
+                arr[i]=arr[i-1]+arr[i-2];
+
+            }
+        
+        return arr[n];
     }
+        
+    
 }
