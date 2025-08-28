5cfba2251f604432f05cdab8ce111bfbffd70e2b
Rushikesh Nakhale <rushikesh.nakhale23@pccoepune.org>
2025-08-28 16:15:02 +0530

    Time: 4 ms (72.77%) | Memory: 45.2 MB (45.95%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -11,13 +11,13 @@ class Solution {
             dp[0][x] = x;
         }
 
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1];
-                } else {
-                    dp[i][j] = 1 + Math.min(dp[i - 1][j - 1], 
-                                            Math.min(dp[i - 1][j], dp[i][j - 1]));
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(word1.charAt(i-1)==word2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1];
+
+                }else{
+                    dp[i][j]=1+Math.min(dp[i-1][j-1],Math.min(dp[i][j-1],dp[i-1][j]));
                 }
             }
         }
