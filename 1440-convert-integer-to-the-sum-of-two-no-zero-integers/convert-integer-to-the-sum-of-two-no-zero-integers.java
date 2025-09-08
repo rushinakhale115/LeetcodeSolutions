785bff43d22a2229ca50d62eae7b7c06cccf4bdb
Rushikesh Nakhale <rushikesh.nakhale23@pccoepune.org>
2025-09-08 23:36:40 +0530

    Time: 1 ms (36.38%) | Memory: 41.5 MB (40.36%) - LeetSync

---
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] getNoZeroIntegers(int n) {
+        for(int a=1;a<n;a++){
+        int b=n-a;
+        if(!String.valueOf(a).contains("0")&& !String.valueOf(b).contains("0")){
+
+        
+        return new int[]{a,b};
+        }
+        }
+        
+        return new int[0];
+    }
+}
