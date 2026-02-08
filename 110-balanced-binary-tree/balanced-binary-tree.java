951785bdc1f592257ae504446d9ca8896ec04750
Rushikesh Nakhale <rushikesh.nakhale23@pccoepune.org>
2026-02-08 23:19:07 +0530

    Time: 0 ms (100.00%) | Memory: 45.7 MB (57.98%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -15,19 +15,22 @@
  */
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        return checkHeight(root)!=-1;
+            return height(root)!=-1;
     }
-    int checkHeight(TreeNode root){
-        if(root==null) return 0;
+    private int height(TreeNode root){
+        if(root==null){
+            return 0;
+        }
 
-        int left=checkHeight(root.left);
-        if(left==-1) return -1;
+        int leftHeight=height(root.left);
+        int rightHeight=height(root.right);
 
-        int right=checkHeight(root.right);
-        if(right==-1) return -1;
+        if(leftHeight==-1 || rightHeight==-1 || Math.abs(leftHeight - rightHeight)>1){
+            return -1;
 
-        if(Math.abs(left-right)>1) return -1;
+        }
+        return Math.max(leftHeight,rightHeight)+1;
 
-        return Math.max(left,right)+1;
+        
     }
 }
