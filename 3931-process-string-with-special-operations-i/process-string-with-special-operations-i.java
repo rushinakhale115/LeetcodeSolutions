aed94684f25eeaa106c8cebe9494f8badbfd4a04
Rushikesh Nakhale <rushikesh.nakhale23@pccoepune.org>
2026-06-16 21:09:34 +0530

    Time: 3 ms (100.00%) | Memory: 55.2 MB (64.71%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder result =new StringBuilder();
+        for(char ch : s.toCharArray()){
+            if(ch=='*'){
+                if(result.length()>0) result.deleteCharAt(result.length()-1);
+            }else if(ch=='#'){
+                result.append(result);
+            }else if(ch=='%'){
+                result=result.reverse();
+            }else{
+                result.append(ch);
+            }
+        }
+        return result.toString();
+
+        
+    }
+}
