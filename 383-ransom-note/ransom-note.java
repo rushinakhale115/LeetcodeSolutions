54f45068b8153e57224362014606222dd4ee3137
Rushikesh Nakhale <rushikesh.nakhale23@pccoepune.org>
2026-06-30 14:27:46 +0530

    Time: 3 ms (49.73%) | Memory: 46.5 MB (56.62%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -2,6 +2,9 @@ class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
         int [] r=new int[26];
         int[]m=new int[26];
+        if(magazine.length()<ransomNote.length()){
+            return false;
+        }
         
         for(int i=0;i<magazine.length();i++){
             m[magazine.charAt(i)-'a']++;
