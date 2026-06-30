4d9ddac7d39bd10e7df8a096d4d4c2ead4fd19e4
Rushikesh Nakhale <rushikesh.nakhale23@pccoepune.org>
2026-06-30 23:14:03 +0530

    Time: 14 ms (62.11%) | Memory: 46.4 MB (30.71%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -1,24 +1,33 @@
 class Solution {
     public int numberOfSubstrings(String s) {
-        int[] counts = new int[3]; 
-        int  j = 0;
+         int count = 0;
+        // Length of the input string
         int n = s.length();
-        for(;j<3;j++){
-            counts[s.charAt(j)-'a']++;
+         int[] freq = new int[3];
+        int left=0;
+
+        // Outer loop to fix the start of the substring
+        for (int right = 0; right< n; right++) {
+            // Array to track the count of 'a', 'b', and 'c'
+            freq[s.charAt(right) - 'a']++;
+            // Inner loop to fix the end of the substring
+            
+                // Update frequency for current character
+                
+
+                // Check if all three characters are present
+                while(freq[0] > 0 && freq[1] > 0 && freq[2] > 0) {
+                    // Add valid substring
+                    count+=(n-right);
+                    freq[s.charAt(left)-'a']--;
+                    left++;
+
+                    
+             }
+            
         }
-        int i = 0;
-        int count = 0;
-        while(j<=n){
-            if (j -i >2 && counts[0]>0 && counts[1]>0 && counts[2]>0) {
-                count+=(n-j)+1;
-                counts[s.charAt(i) - 'a']--;
-                i++;
-            }
-            else{
-                if(j<n) counts[s.charAt(j) - 'a']++;
-                j++;
-            }
-        }   
+
         return count;
+        
     }
 }
