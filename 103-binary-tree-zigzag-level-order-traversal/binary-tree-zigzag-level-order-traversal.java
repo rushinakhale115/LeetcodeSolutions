515a6b3ba0e4fc66ebf09ead9540167aded0a1bd
Rushikesh Nakhale <rushikesh.nakhale23@pccoepune.org>
2026-07-01 12:52:40 +0530

    Time: 1 ms (79.44%) | Memory: 43.9 MB (9.30%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -13,29 +13,44 @@
  *     }
  * }
  */
-public class Solution {
-    public List<List<Integer>> zigzagLevelOrder(TreeNode root) 
-    {
-        List<List<Integer>> sol = new ArrayList<>();
-        travel(root, sol, 0);
-        return sol;
-    }
-    
-    private void travel(TreeNode curr, List<List<Integer>> sol, int level)
-    {
-        if(curr == null) return;
-        
-        if(sol.size() <= level)
-        {
-            List<Integer> newLevel = new LinkedList<>();
-            sol.add(newLevel);
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        //your code goes here
+        List<List<Integer>> res=new ArrayList<>();
+        if(root==null) return res;
+        Queue<TreeNode> q=new LinkedList<>();
+        q.offer(root);
+
+        boolean LeftToRight=true;
+
+        while(!q.isEmpty()){
+            int len=q.size();
+            List<Integer> level = new ArrayList<>();
+
+            for(int i=0;i<len;i++){
+            TreeNode node=q.poll();
+
+            if(LeftToRight){
+                level.add(node.val);
+            }
+            else{
+                level.add(0,node.val);
+            }
+            if(node.left!=null){
+                q.offer(node.left);
+            }
+            if(node.right!=null){
+                q.offer(node.right);
+            }
+            }
+            res.add(level);
+            LeftToRight=!LeftToRight;
+
+           
+        }
+        return res;
         }
         
-        List<Integer> collection  = sol.get(level);
-        if(level % 2 == 0) collection.add(curr.val);
-        else collection.add(0, curr.val);
-        
-        travel(curr.left, sol, level + 1);
-        travel(curr.right, sol, level + 1);
+
+
     }
-}
