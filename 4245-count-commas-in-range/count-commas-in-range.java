4be7ffc60d0effcb4cd50630237b3dc4fb42e2f3
Rushikesh Nakhale <rushikesh.nakhale23@pccoepune.org>
2026-09-08 23:38:02 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (72.85%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.java
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -1,9 +1,8 @@
 class Solution {
     public int countCommas(int n) {
         int count=0;
-        if(n<999) return 0;
-        else if(n<=99999) return n-999;
-        else return 99001;
+        if(n<1000) return 0;
+        else return n-999;
 
     }
 }
